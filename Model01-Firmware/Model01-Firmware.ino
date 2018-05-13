bcd8c6309bef235fd5396a1f9721a72c5014b4d2
Hiraku NAKANO <hiraku@tojiru.net>
2018-05-13 15:49:17 +0900

    update

--- Model01-Firmware/Model01-Firmware.ino
+++ Model01-Firmware/Model01-Firmware.ino
@@ -128,18 +128,18 @@ enum { QWERTY, NUMPAD, FUNCTION }; // layers
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [QWERTY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+  (___,             Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Tab,         Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Backspace,
    Key_LeftControl, Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_LeftShift,   Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+   Key_Lang2, Key_Spacebar, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   LockLayer(NUMPAD), Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Minus,
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_International1,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+   LockLayer(NUMPAD),  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_Minus,
+   Key_International3, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_LeftBracket,
+                       Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
+   Key_Equals,         Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_International1,
+   Key_RightAlt, Key_Enter, Key_Spacebar, Key_Lang1,
    ShiftToLayer(FUNCTION)),
 
 
@@ -166,10 +166,10 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    ___, Key_Delete, ___, ___,
    ___,
 
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,           Key_F10,       Key_F11,
+   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_RightBracket, Key_Backslash, Key_F12,
+                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,   ___,           ___,
+   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,              Key_Backslash, Key_Pipe,
    ___, ___, Key_Enter, ___,
    ___)
 
